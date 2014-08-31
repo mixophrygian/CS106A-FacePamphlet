cf2671306b73d51a3e18ba160724af0e18c208d5
S <s>
2014-08-31 15:18:27 -0700

    {"type":"TI", "runs":"1"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -31,7 +31,7 @@ public class FacePamphletCanvas extends GCanvas
 	 * passed in.
 	 */
 	public void showMessage(String msg) {
-		remove(message);
+		
 		message.setLabel(msg);
 		message.move(getWidth()/2, getHeight() - BOTTOM_MESSAGE_MARGIN);
 		message.setFont(MESSAGE_FONT);
