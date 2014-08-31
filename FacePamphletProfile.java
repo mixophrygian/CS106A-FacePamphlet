0f025065272ec948dc44c8150a25d8cbfc25e4e1
S <s>
2014-08-31 11:26:48 -0700

    {"type":"TI", "runs":"0"}

--- FacePamphletProfile.java
+++ FacePamphletProfile.java
@@ -123,10 +123,10 @@ public class FacePamphletProfile implements FacePamphletConstants {
 	public String toString() {
 		//Might be a neater way to display friendsList, perhaps a for Loop or iterator//
 		String neatFriendsList = " ";
-		while(getFriends().hasNext()){
-			String nextFriend = getFriends().next();
+		for(int i = 0; i < friendsList.size(); i++){
+			String nextFriend = friendsList.get(i);
 			neatFriendsList = neatFriendsList + nextFriend + ", ";
-			getFriends().remove();
+			i++;
 		}
 		
 		return ""+profileName+ " "+status+":" + neatFriendsList + " ";
