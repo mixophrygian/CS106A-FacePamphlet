b051453f035db11de711a59d1ad37fa4cb5e5c05
S <s>
2014-09-02 18:28:50 -0700

    {"type":"TI", "runs":"1"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -57,7 +57,7 @@ public class FacePamphlet extends Program
 		
 		//Adds a canvas to the center of the window//
 		add(canvas);
-		
+		canvas.showMessage("Click Add to create a new profile!");
 		
 		//Adds an empty database (hashmap of strings and FacePamphlet Profiles)//
 		database = new FacePamphletDatabase();
@@ -67,7 +67,7 @@ public class FacePamphlet extends Program
 		
 		addActionListeners();
 		
-		canvas.showMessage("Click Add to create a new profile!");
+		
     }
     
   
