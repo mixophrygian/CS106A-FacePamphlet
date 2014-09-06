4b7404ccef4f5eb4976fd599b1f28220b44f8773
S <s>
2014-09-05 18:30:47 -0700

    {"type":"TI", "runs":"1"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -165,10 +165,10 @@ public class FacePamphlet extends Program
     				}
     				currentProfile.setImage(image);
     				canvas.displayProfile(currentProfile);
-    				canvas.showMessage("The picture for " +currentProfile.getName() + " has been set to " + file.toString());
     				String shortFileName = file.toString();
     				shortFileName = shortFileName.substring(file.toString().lastIndexOf("/") + 1);
     				pictureField.setText(shortFileName);
+    				canvas.showMessage("The picture for " +currentProfile.getName() + " has been set to " + shortFileName);
     			}
     
     		}else{
