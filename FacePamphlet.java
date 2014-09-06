91657a19880ce65219554c8def40198a498dc7c9
S <s>
2014-09-05 18:53:15 -0700

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -44,7 +44,7 @@ public class FacePamphlet extends Program
 		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
 		
 		//Adds picture button and label to the left of the window
-		pictureField = new JLabel("No Image", pictureField.CENTER);
+		pictureField = new JLabel("No Image", JLabel.CENTER);
 		add(pictureField, WEST);
 		ChangePicture = new JButton("Change Picture");
 		add(ChangePicture, WEST);
